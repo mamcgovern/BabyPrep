73ca697bdfc11d33f6926f77282fb62288fd3fc1
Maddelynne McGovern <maddelynnemcgovern@gmail.com>
2026-09-16 14:42:38 -0500

    updated firebase

--- baby-prep/src/services/firebase.js
+++ baby-prep/src/services/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCB8uyxP7O2ScuQLBFfGwCC4e1WJOeMm9A",
+  authDomain: "baby-prep-4ebb3.firebaseapp.com",
+  projectId: "baby-prep-4ebb3",
+  storageBucket: "baby-prep-4ebb3.firebasestorage.app",
+  messagingSenderId: "708877422563",
+  appId: "1:708877422563:web:984c3ec1dd5b7611071ad6",
 };
 
 const app = initializeApp(firebaseConfig);
